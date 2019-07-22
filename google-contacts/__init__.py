13bbe16e67c9210cf79a0663bfdcbcef347821f1
Christopher DiTusa <cditusa@gmail.com>
2019-07-21 22:25:48 -0400

    Add create contact function
    Tested, working -- without field validation.
    
    Also in a future release we want to validate that the contact
    does not already exist on the target mailbox.

--- google-contacts/__init__.py
+++ google-contacts/__init__.py
@@ -2,11 +2,15 @@
 """Add/Edit/Remove contacts from Google Contacts"""
 
 #from __future__ import print_function
+from __future__ import print_function
+import pickle
+import os.path
 import tkinter as tk
 import logging
 from googleapiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+#from httplib2 import Http
 
 # Logging format
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -17,20 +21,13 @@ class GoogleContactGUI:
         'Mobile Phone', 'Home Phone'
     ]
 
-    contact_body = {
-        'names': [{}],
-        'phoneNumbers': [{}, {}, {}],
-        'organizations': [{}],
-        'emailAddresses': [{}]
-    }
-
     def __init__(self, master):
         self.master = master
         self.master.title("Google Contacts API v0.3")
-        self.scopes = ['https://www.googleapis.com/auth/contacts'] # If modifying these scopes, delete the file token.json.
-        self.token = 'token.json'
-        self.credential = 'credential.json'
-        self.service = self.authenticate(self.token)
+        # If modifying these scopes, delete the file token.json.
+        self.scopes = ['https://www.googleapis.com/auth/contacts']
+        self.credentials = 'credentials.json'
+        self.service = self._authenticate(self.credentials)
         self.container = tk.Frame
         self.logger = logging.getLogger('contactGUI')
 
@@ -45,69 +42,75 @@ class GoogleContactGUI:
             self.entry.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
             self.entries.append((field, self.entry))
         self.create_button = tk.Button(self.master, text='Create Contact',
-            command=(lambda s=self.service, e=self.entries: self.create_contact(s, e)))
+            command=(lambda s=self.service, e=self.entries: self._create_contact(s, e)))
         self.create_button.pack(side=tk.LEFT, padx=5, pady=5)
         self.quit_button = tk.Button(self.master, text='Quit', command=self.master.quit)
         self.quit_button.pack(side=tk.LEFT, padx=40)
 
-    def authenticate(self, token):
-        import os
-        if os.path.exists(token):
-            cred_store = file.Storage(token)
-            credential = cred_store.get()
-        if not credential or credential.invalid:
-            flow = client.flow_from_clientsecrets(self.credential, self.scopes)
-            credential = tools.run_flow(flow, cred_store)
-        service = build('people', 'v1', http=credential.authorize(Http()),
-            cache_discovery=False)
+    def _authenticate(self, cred_file):
+        """Authenticate against Google Contact API"""
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    cred_file, self.scopes)
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        service = build('people', 'v1', credentials=creds, cache_discovery=False)
         return service
 
-    def create_contact(self, service, entries):
+    def _create_contact(self, service, entries):
+        contact_body = self._fill_form(entries)
+        contact = service.people().createContact(body=contact_body)
+        contact.execute()
+        logging.info('Contact created successfully')
+
+    def _fill_form(self, entries):
+        """Fills in the contact body with info from Tkinter fields"""
+        contact_body = {
+            'names': [{}],
+            'phoneNumbers': [{}, {}, {}],
+            'organizations': [{}],
+            'emailAddresses': [{}]
+        }
         count = 0
         for entry in entries:
             key, value = entry[0], entry[1].get()
             if value is not None:
                 if key == 'First Name':
-                    self.contact_body['names'][0]['givenName'] = value
-                    self.contact_body['names'][0]['displayName'] = value
+                    contact_body['names'][0]['givenName'] = value
+                    contact_body['names'][0]['displayName'] = value
                 elif key == 'Last Name':
-                    self.contact_body['names'][0]['familyName'] = value
-                    self.contact_body['names'][0]['displayName'] += ' ' + value
+                    contact_body['names'][0]['familyName'] = value
+                    contact_body['names'][0]['displayName'] += ' ' + value
                 elif key == 'Company':
-                    self.contact_body['organizations'][0]['name'] = value
+                    contact_body['organizations'][0]['name'] = value
                 elif key == 'Email Address':
-                    self.contact_body['emailAddresses'][0]['value'] = value
-                    self.contact_body['emailAddresses'][0]['type'] = 'work'
+                    contact_body['emailAddresses'][0]['value'] = value
+                    contact_body['emailAddresses'][0]['type'] = 'work'
                 elif key == 'Business Phone':
-                    self.contact_body['phoneNumbers'][count]['value'] = value
-                    self.contact_body['phoneNumbers'][count]['type'] = 'work'
+                    contact_body['phoneNumbers'][count]['value'] = value
+                    contact_body['phoneNumbers'][count]['type'] = 'work'
                     count += 1
                 elif key == 'Mobile Phone':
-                    self.contact_body['phoneNumbers'][count]['value'] = value
-                    self.contact_body['phoneNumbers'][count]['type'] = 'mobile'
+                    contact_body['phoneNumbers'][count]['value'] = value
+                    contact_body['phoneNumbers'][count]['type'] = 'mobile'
                     count += 1
                 elif key == 'Home Phone':
-                    self.contact_body['phoneNumbers'][count]['value'] = value
-                    self.contact_body['phoneNumbers'][count]['type'] = 'home'
+                    contact_body['phoneNumbers'][count]['value'] = value
+                    contact_body['phoneNumbers'][count]['type'] = 'home'
                     count += 1
-        contact = service.people().createContact(body=self.contact_body)
-        contact.execute()
-        logging.info('Contact {} created'.format())
+        return contact_body
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     contact_gui = GoogleContactGUI(root)
     root.mainloop()
-    """
-    service = google_auth()
-    root = tkinter.Tk()
-    root.title('Google Contacts v0.2')
-    ents = make_form(root, fields)
-    root.bind('<Return>', (lambda event, s=service, e=ents: create_contact(s, e)))
-    b1 = tkinter.Button(root, text='Create Contact', command=(lambda s=service, e=ents: create_contact(s, e)))
-    b1.pack(side=tkinter.LEFT, padx=5, pady=5)
-    b2 = tkinter.Button(root, text='Quit', command=root.quit)
-    b2.pack(side=tkinter.LEFT, padx=5, pady=5)
-    root.mainloop()
-    """
+    
